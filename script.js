0a350001386abed82b6409167b09d76148d8294b
Zookegger <nguyenductrung11122004@gmail.com>
2024-10-02 23:21:17 +0700

    Refactored code, added UI

--- script.js
+++ script.js
@@ -1,34 +1,84 @@
+// Create buttons for player choices
+const rockButton = document.createElement("button");
+rockButton.innerText = "Rock";
+rockButton.style.cssText = "font-size: 32px; padding: 16px; margin: 0 16px;"
+
+const paperButton = document.createElement("button");
+paperButton.innerText = "Paper";
+paperButton.style.cssText = "font-size: 32px; padding: 16px; margin: 0 16px;"
+
+const scissorButton = document.createElement("button");
+scissorButton.innerText = "Scissor";
+scissorButton.style.cssText = "font-size: 32px; padding: 16px; margin: 0 16px;"
+
+// Initialize scores
+let playerScore = 0;
+let computerScore = 0;
+
+// Create scoreboard
+const scoreBoard = document.createElement("div");
+scoreBoard.style.cssText = "border: 3px solid black";
+
+const playerScoreBoard = document.createElement("div");
+playerScoreBoard.innerText = "Player: 0";
+const computerScoreBoard = document.createElement("div");
+computerScoreBoard.innerText = "Computer: 0";
+
+scoreBoard.appendChild(playerScoreBoard);
+scoreBoard.appendChild(computerScoreBoard);
+
+// Funtion to get random computer choice
 function getComputerChoice() {
-    let answer = Math.floor(Math.random() * 2);
-    if (answer === 0)
-        return 'rock';
-    else if (answer === 1)
-        return 'paper';
-    else
-        return 'scissor';
+    const answer = ['rock', 'paper', 'scissor'];
+    return answer[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-    return answer = prompt("Rock paper scissor").toLowerCase();
-}
-
+// Implement game logic, returns 1 if players wins, returns 0 if draw and return -1 if players loses
 function gameLogic(p1, p2) {
-    console.log(p1, p2);
-    if (p1 === 'rock' && p2 === 'rock')
-        alert("Draw");
-    else if (p1 === 'paper' && p2 === 'paper')
-        alert("Draw");
-    else if (p1 === 'scissor' && p2 === 'scissor')
-        alert("Draw");
-    else if (p1 === 'rock' && p2 === 'scissor')
-        alert("You win");
-    else if (p1 === 'paper' && p2 === 'rock')
-        alert("You Win");
-    else if (p1 === 'scissor' && p2 === 'paper')
-        alert("You Win");
+    if (p1 === 'rock' && p2 === 'scissor' ||
+        p1 === 'paper' && p2 === 'rock' ||
+        p1 === 'scissor' && p2 === 'paper')
+        return 1;
+    else if (p1 === p2)
+        return 0;
     else
-        alert("You Lose");
+        return -1;
 }
 
-for (i = 0; i < 5; i++)
-    gameLogic(getHumanChoice(), getComputerChoice());
+function updateScore() {
+    playerScoreBoard.innerText = `Player: ${playerScore}`;
+    computerScoreBoard.innerText = `Computer: ${computerScore}`;
+}
+
+
+function playRound(playerChoice) {
+    const computerChoice = getComputerChoice();
+    let result = gameLogic(playerChoice, computerChoice);
+
+    console.log(playerChoice);
+    console.log(computerChoice);
+
+    if (result === 1){
+        playerScore++;
+        console.log("Player wins!");
+    } else if (result === -1) {
+        computerScore++;
+        console.log("Computer wins!");
+    } else {
+        console.log("It's a tie!");
+    } 
+    
+    updateScore();
+}
+
+// Event listeners for player's button input
+rockButton.addEventListener('click', () => playRound("rock"));
+paperButton.addEventListener('click', () => playRound("paper"));
+scissorButton.addEventListener('click', () => playRound("scissor"));
+
+// Add button and scoreboard to html body
+const body = document.querySelector("body");
+body.appendChild(rockButton);
+body.appendChild(paperButton);
+body.appendChild(scissorButton);
+body.appendChild(scoreBoard);
